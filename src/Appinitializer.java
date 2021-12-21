a419aaa31d9f81ced2e1dff0f8e756d008ec9973
Radintha Dilshan <rasinthadilshan9@gmail.com>
2021-12-21 07:16:53 +0530

    'Create Main Page'

--- src/Appinitializer.java
+++ src/Appinitializer.java
@@ -1,5 +1,11 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class Appinitializer extends Application {
 
@@ -8,7 +14,12 @@ public class Appinitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/LoginPage.fxml"));
+        Scene mainScene = new Scene(root);
+        primaryStage.setScene(mainScene);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
     }
 }
---
+++ src/controller/MainPageController.java
@@ -0,0 +1,139 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class MainPageController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtStudentId;
+
+    @FXML
+    private TextField txtLastName;
+
+    @FXML
+    private TextField txtFirstName;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtContact;
+
+    @FXML
+    private ComboBox<?> cmbProgram;
+
+    @FXML
+    private Button btnSclear;
+
+    @FXML
+    private Button btnSadd;
+
+    @FXML
+    private Button btnSupdate;
+
+    @FXML
+    private Button btnSdelete;
+
+    @FXML
+    private TextField txtProgramId;
+
+    @FXML
+    private TextField txtDuration;
+
+    @FXML
+    private TextField txtProgram;
+
+    @FXML
+    private TextField txtFee;
+
+    @FXML
+    private Button btnPclear;
+
+    @FXML
+    private Button btnPadd;
+
+    @FXML
+    private Button btnPupdate;
+
+    @FXML
+    private Button btnPdelete;
+
+    @FXML
+    private TableView tblDetails;
+
+    @FXML
+    private TableColumn colStudentId;
+
+    @FXML
+    private TableColumn colFirstName;
+
+    @FXML
+    private TableColumn colLastName;
+
+    @FXML
+    private TableColumn colProgramId;
+
+    @FXML
+    private TableColumn colProgram;
+
+    @FXML
+    private TableColumn colFee;
+
+    @FXML
+    private TableColumn colDuration;
+
+    @FXML
+    void programAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void programClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void programDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void programUpdateOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void studentAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void studentClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void studentDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void studentUpdateOnAction(ActionEvent event) {
+
+    }
+
+}

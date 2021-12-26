91fa31903b6d4db7e02dc2a49a122f9836202045
Radintha Dilshan <rasinthadilshan9@gmail.com>
2021-12-26 13:10:44 +0530

    'Create DTO Clasess'

--- src/dto/RegisterDetailDTO.java
+++ src/dto/RegisterDetailDTO.java
@@ -1,4 +1,42 @@
 package dto;
 
+import java.time.LocalDate;
+
 public class RegisterDetailDTO {
+    private String sId;
+    private String pId;
+    private LocalDate date;
+
+    public RegisterDetailDTO() {
+    }
+
+    public RegisterDetailDTO(String sId, String pId, LocalDate date) {
+        this.setsId(sId);
+        this.setpId(pId);
+        this.setDate(date);
+    }
+
+    public String getsId() {
+        return sId;
+    }
+
+    public void setsId(String sId) {
+        this.sId = sId;
+    }
+
+    public String getpId() {
+        return pId;
+    }
+
+    public void setpId(String pId) {
+        this.pId = pId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
--- src/dto/StudentDTO.java
+++ src/dto/StudentDTO.java
@@ -1,4 +1,82 @@
 package dto;
 
 public class StudentDTO {
+    private String sId;
+    private String sName;
+    private String nic;
+    private String address;
+    private String contact;
+    private String program;
+
+    public StudentDTO() {
+    }
+
+    public StudentDTO(String sId, String sName, String nic, String address, String contact, String program) {
+        this.setsId(sId);
+        this.setsName(sName);
+        this.setNic(nic);
+        this.setAddress(address);
+        this.setContact(contact);
+        this.setProgram(program);
+    }
+
+    public String getsId() {
+        return sId;
+    }
+
+    public void setsId(String sId) {
+        this.sId = sId;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "sId='" + sId + '\'' +
+                ", sName='" + sName + '\'' +
+                ", nic='" + nic + '\'' +
+                ", address='" + address + '\'' +
+                ", contact='" + contact + '\'' +
+                ", program='" + program + '\'' +
+                '}';
+    }
 }

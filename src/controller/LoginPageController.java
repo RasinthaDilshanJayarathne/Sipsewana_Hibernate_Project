fda3a2eef3107bcb80d2cbdb80ca424fd7bd7fd2
Radintha Dilshan <rasinthadilshan9@gmail.com>
2021-12-26 06:41:10 +0530

    'Create LoginPage.fxml'

--- src/controller/LoginPageController.java
+++ src/controller/LoginPageController.java
@@ -1,7 +1,23 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class LoginPageController {
     public AnchorPane root;
+    public TextField txtUserName;
+    public PasswordField txtPassword;
+    public Label lblError;
+
+    public void goToPassword(ActionEvent actionEvent) {
+    }
+
+    public void goToLoginPage(ActionEvent actionEvent) {
+    }
+
+    public void goToCanclePage(ActionEvent actionEvent) {
+    }
 }

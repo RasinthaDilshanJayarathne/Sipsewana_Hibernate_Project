77a3a5aeff5636c7723d8f9d6dad42459ffb7206
Radintha Dilshan <rasinthadilshan9@gmail.com>
2021-12-31 10:46:29 +0530

    'Create BOImpl clases'

--- src/bo/impl/ProgramBOImpl.java
+++ src/bo/impl/ProgramBOImpl.java
@@ -4,6 +4,7 @@ import bo.custom.ProgramBO;
 import dao.custom.ProgramDAO;
 import dao.impl.DAOFactory;
 import dto.ProgramDTO;
+import entity.Program;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -14,31 +15,36 @@ public class ProgramBOImpl implements ProgramBO {
 
     @Override
     public ArrayList<ProgramDTO> getAllProgram() throws SQLException, ClassNotFoundException {
-        return null;
+        ArrayList<ProgramDTO> allCourses = new ArrayList<>();
+        ArrayList<Program> all = programDAO.getAll();
+        for (Program program : all) {
+            allCourses.add(new ProgramDTO(program.getpId(),program.getpName(),program.getDuration(),program.getFee()));
+        }
+        return allCourses;
     }
 
     @Override
-    public boolean addProgram(ProgramDTO studentDTO) throws SQLException, ClassNotFoundException {
-        return false;
+    public boolean addProgram(ProgramDTO programDTO) throws SQLException, ClassNotFoundException {
+        return programDAO.add(new Program(programDTO.getpId(),programDTO.getpName(),programDTO.getDuration(),programDTO.getFee()));
     }
 
     @Override
-    public boolean updateProgram(ProgramDTO studentDTO) throws SQLException, ClassNotFoundException {
-        return false;
+    public boolean updateProgram(ProgramDTO programDTO) throws SQLException, ClassNotFoundException {
+        return programDAO.update(new Program(programDTO.getpId(),programDTO.getpName(),programDTO.getDuration(),programDTO.getFee()));
     }
 
     @Override
     public boolean ifProgramExist(String id) throws SQLException, ClassNotFoundException {
-        return false;
+        return programDAO.ifCourseExist(id);
     }
 
     @Override
     public boolean deleteProgram(String id) throws SQLException, ClassNotFoundException {
-        return false;
+        return programDAO.delete(id);
     }
 
     @Override
     public String generateNewID() throws SQLException, ClassNotFoundException {
-        return null;
+        return programDAO.generateNewID();
     }
 }

eaa4b7661d32b308a30157dc5fbfdefd42a3e351
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-01-01 19:03:00 +0530

    'Finished Validation'

--- src/util/validation/ValidationUtil.java
+++ src/util/validation/ValidationUtil.java
@@ -2,6 +2,7 @@ package util.validation;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 import java.util.LinkedHashMap;
 import java.util.regex.Pattern;
@@ -13,11 +14,13 @@ public class ValidationUtil {
             Pattern patternValue = map.get(textFieldKey);
             if (!patternValue.matcher(textFieldKey.getText()).matches()) {
                 if (!textFieldKey.getText().isEmpty()) {
-                    textFieldKey.setStyle("-fx-border-color: red");
+                    textFieldKey.setStyle("-fx-text-fill: red");
+                    ((AnchorPane) textFieldKey.getParent()).getChildren().get(1).setStyle("-fx-text-fill: red");
                 }
                 return textFieldKey;
             }
-            textFieldKey.setStyle("-fx-border-color: green");
+            textFieldKey.setStyle("-fx-text-fill: green");
+            ((AnchorPane) textFieldKey.getParent()).getChildren().get(1).setStyle("-fx-text-fill: green");
         }
         btn.setDisable(false);
         return true;
